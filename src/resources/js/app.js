b3be049970c53cc3569dcb332265a73ad6263532
segamiken <ken.s.vollyball@gmail.com>
2020-04-26 00:44:14 +0900

    LaravelでVue.jsを使えるようにした

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+window.Vue = require('vue'); 
+
+Vue.component('example', require('./components/Example.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ src/resources/js/components/Example.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        yay
+    </div>
+</template>
